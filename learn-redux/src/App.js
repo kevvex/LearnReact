8f8006fb13ff32e1d5b7f737d106cb7e99bdcefe
kevvex <kevinljung98@gmail.com>
2021-11-19 20:51:01 +0100

    Completed tutorial
    Redux For Beginners | React Redux Tutorial

--- learn-redux/src/App.js
+++ learn-redux/src/App.js
@@ -3,10 +3,19 @@
 * Title: Redux For Beginners | React Redux Tutorial
 * https://youtu.be/CVpUuw9XSjY
 */
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './actions';
 function App() {
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <h1>Learn Redux</h1>
+      <h1>Counter: {counter}</h1>
+      <button onClick={() => dispatch(increment(5))}>+</button>
+      <button onClick={() => dispatch(decrement(5))}>-</button>
+      {isLogged ? <h1>Valuable Information I shouldn't see</h1> : ''}
     </div>
   );
 }
--- learn-redux/src/index.js
+++ learn-redux/src/index.js
@@ -3,48 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {createStore} from 'redux';
+import { createStore } from 'redux';
+import allReducer from './reducers';
+import { Provider } from 'react-redux';
 
-// STORE -> GLOBALIZED STATE
-
-// ACTION INCREMENT
-const increment = () => {
-  return {
-    type: 'INCREMENT'
-  }
-}
-
-const decrement = () => {
-  return {
-    type: 'DECREMENT'
-  }
-}
-
-// REDUCER
-const counter = (state = 0, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1;
-    case 'DECREMENT':
-      return state - 1;
-  }
-}
-
-let store = createStore(counter);
-
-// Display it in the console
-store.subscribe(() => console.log(store.getState()));
-
-// DISPATCH
-store.dispatch(increment());
-
-
-store.dispatch(decrement());
-store.dispatch(decrement());
+const store = createStore(
+  allReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
