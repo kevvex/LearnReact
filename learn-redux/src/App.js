60979015ed5bf619c084b67060ad94a6ed50e631
kevvex <kevinljung98@gmail.com>
2021-11-19 19:53:49 +0100

    Initial commit for a React project

---
+++ learn-redux/src/App.js
@@ -0,0 +1,14 @@
+/*
+* Youtube Channel: Dev Ed
+* Title: Redux For Beginners | React Redux Tutorial
+* https://youtu.be/CVpUuw9XSjY
+*/
+function App() {
+  return (
+    <div className="App">
+      <h1>Learn Redux</h1>
+    </div>
+  );
+}
+
+export default App;
